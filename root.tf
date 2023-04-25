6353f47adaa78e5a78a9693a322a80628231ab2f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-04-25 21:02:17 +0100

    Add a role to allow the terraform for Github repos to access the state file

--- root.tf
+++ root.tf
@@ -2,15 +2,31 @@ locals {
   terraform_state_bucket_name = "mgmt-dp-terraform-state"
 }
 module "terraform_s3_bucket" {
-  source                = "git::https://github.com/nationalarchives/da-terraform-modules.git//s3"
+  source                = "git::https://github.com/nationalarchives/da-terraform-modules.git//s3?ref=add-s3-module"
   bucket_name           = local.terraform_state_bucket_name
   bucket_policy         = templatefile("${path.module}/templates/s3/s3_secure_transport.json.tpl", { bucket_name = local.terraform_state_bucket_name })
   logging_bucket_policy = templatefile("${path.module}/templates/s3/s3_secure_transport_logging.json.tpl", { bucket_name = "${local.terraform_state_bucket_name}-logs" })
 }
 
 module "terraform_dynamo" {
-  source        = "git::https://github.com/nationalarchives/da-terraform-modules.git//dynamo"
+  source        = "git::https://github.com/nationalarchives/da-terraform-modules.git//dynamo?ref=add-s3-module"
   hash_key      = "LockID"
   hash_key_type = "S"
   table_name    = "mgmt-dp-terraform-state-lock"
+}
+
+module "terraform_github_repository_iam" {
+  source             = "git::https://github.com/nationalarchives/tdr-terraform-modules.git//iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", { account_id = data.aws_caller_identity.current.account_id, repo_filter = "dp-terraform-github-repositories" })
+  common_tags        = {}
+  name               = "MgmtDPTerraformGitHubRepositoriesRole"
+  policy_attachments = {
+    state_access_policy = module.terraform_github_repository_policy.policy_arn
+  }
+}
+
+module "terraform_github_repository_policy" {
+  source        = "git::https://github.com/nationalarchives/tdr-terraform-modules.git//iam_policy"
+  name          = "MgmtDPTerraformGitHubRepositoriesPolicy"
+  policy_string = templatefile("${path.module}/templates/iam_policy/terraform_state_access.json.tpl", { bucket_name = local.terraform_state_bucket_name })
 }
