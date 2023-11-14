aa12e4a870afd57d48f227da630b532970981e2a
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-11-14 10:45:15 +0000

    DR2-1330 Add repository vulnerability notifications.
    This adds the Slack API destination and an eventbridge rule to the
    management account.

--- root_data.tf
+++ root_data.tf
@@ -11,3 +11,12 @@ data "aws_ssm_parameter" "staging_account_number" {
 data "aws_ssm_parameter" "prod_account_number" {
   name = "/mgmt/prod_account_number"
 }
+
+data "aws_ssm_parameter" "slack_token" {
+  name            = "/mgmt/slack/token"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "dr2_notifications_slack_channel" {
+  name = "/mgmt/slack/notifications/channel"
+}
