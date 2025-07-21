7395b593e408e83554184c7bcf6cd39bf8777a4d
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-07-21 11:06:35 +0100

    Remove flatten

--- github_repository_filters/locals.tf
+++ github_repository_filters/locals.tf
@@ -6,9 +6,9 @@ locals {
     ]), local.repository_branch_filters)
   }
   repository_branch_filters = [
-    for repository in var.repositories : flatten([
+    for repository in var.repositories : [
       "repo:${var.organisation}/${repository.name}:ref:refs/heads/${repository.branch}",
       "repo:${var.organisation}/${repository.name}:pull_request"
-    ])
+    ]
   ]
 }
